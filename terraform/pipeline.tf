8c21e8a85b413854a1dc175977f568a9ec49d2c2
Jagyanjit Rout <ece.22beci74@silicon.ac.in>
2025-07-28 18:19:01 +0530

    Create pipeline.tf

---
+++ terraform/pipeline.tf
@@ -0,0 +1,66 @@
+resource "aws_codepipeline" "codepipeline" {
+  name     = "project1-pipeline"
+  role_arn = data.aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "SourceAction"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner                = "jagyanjit"
+        Repo                 = "DevopsProject1"
+        Branch               = "main"
+        OAuthToken           = var.github_token
+        PollForSourceChanges = true
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "BuildAction"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.codebuild.name
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "DeployAction"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "CodeDeploy"
+      input_artifacts = ["build_output"]
+      version         = "1"
+
+      configuration = {
+        ApplicationName     = aws_codedeploy_app.app.name
+        DeploymentGroupName = aws_codedeploy_deployment_group.app_deploy_group.deployment_group_name
+      }
+    }
+  }
+}
